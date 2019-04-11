9092beca48c6b7c812aec62492b8670612acb320
Oswin Frans <oswfrans@gmail.com>
2019-04-11 14:45:18 +0200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -17,9 +17,12 @@ use gotham::helpers::http::response::create_response;
 use gotham::router::{builder::*, Router};
 use gotham::state::{FromState, State};
 
-///update main to handle both say_hello and get request
-///
+/// [x] update to handle both say_hello and get request
 
+///update to handle simple schema
+/// get or create simple mleap model that takes said schema
+
+/// update get_product_handler to output score from mleap
 
 const HELLO_WORLD: &'static str = "Hello World!";
 
@@ -35,18 +38,67 @@ struct Product {
     name: String,
 }
 
-/// Create a `Handler` which is invoked when responding to a `Request`.
-///
-/// How does a function become a `Handler`?.
-/// We've simply implemented the `Handler` trait, for functions that match the signature used here,
-/// within Gotham itself.
-pub fn say_hello(state: State) -> (State, &'static str) {
-    (state, HELLO_WORLD)
+fn get_product_handler(mut state: State) -> (State, Response<Body>) {
+    let res = {
+        // Access the `QueryStringExtractor` instance from `state` which was put there for us by the
+        // `Router` during request evaluation.
+        //
+        // As well as permitting storage in `State` by deriving `StateData` our query string
+        // extractor struct automatically gains the `take_from` method and a number of other
+        // methods via the `gotham::state::FromState` trait.
+        //
+        // n.b. Once taken out of `state` values can no longer be accessed by other application
+        // code or middlewares.
+        let query_param = QueryStringExtractor::take_from(&mut state);
+
+        let product = Product {
+            name: query_param.name,
+        };
+        create_response(
+            &state,
+            StatusCode::OK,
+            mime::APPLICATION_JSON,
+            serde_json::to_vec(&product).expect("serialized product"),
+        )
+    };
+    (state, res)
 }
 
-/// Start a server and call the `Handler` we've defined above for each `Request` we receive.
+fn say_hello(state: State) -> (State, Response<Body>) {
+
+    // create the response
+    let res = create_response(&state, StatusCode::OK, mime::TEXT_PLAIN, "hello");
+
+    // done!
+    (state, res)
+}
+
+/// Create a `Router`
+
+fn router() -> Router {
+    build_simple_router(|route| {
+        route
+            .post("/products")
+            // This tells the Router that for requests which match this route that query string
+            // extraction should be invoked storing the result in a `QueryStringExtractor` instance.
+            .with_query_string_extractor::<QueryStringExtractor>()
+            .to(get_product_handler);
+        route
+            .get("/health")
+            .to(say_hello);
+    })
+}
+
+/// Start a server and use a `Router` to dispatch requests
 pub fn main() {
     let addr = "127.0.0.1:7878";
     println!("Listening for requests at http://{}", addr);
-    gotham::start(addr, || Ok(say_hello))
+    gotham::start(addr, router())
 }
+
+/// Start a server and call the `Handler` we've defined above for each `Request` we receive.
+//pub fn main() {
+//    let addr = "127.0.0.1:7878";
+//    println!("Listening for requests at http://{}", addr);
+//    gotham::start(addr, || Ok(say_hello))
+//}
